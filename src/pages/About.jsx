0ca500f92b7d9363fff289168d8111e5e20f2e5b
Snehasish Das <78856467+snehasishdasz@users.noreply.github.com>
2023-08-30 11:04:48 +0530

    About me done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,11 +11,117 @@ import BootStrap from '../assets/bootstrap.png'
 import NextJS from '../assets/next.webp'
 import JavaScript from '../assets/javascript.png'
 import Sass from '../assets/sass.png'
+import Imu from '../assets/imu.png';
+import Terminal from '../assets/terminal.gif';
 
 
 const About = () => (
-   <section>
-    
+   <section className="about" id="about">
+   <div className="about-me">
+      <h2 className="title about-title">aBOUT mE</h2>
+      <div className="aboutImageContainer">
+      <img src={Terminal} alt="snehasish" className='hero-image' />
+      </div>
+      <button className="hire-me">
+        <a href="https://drive.google.com/file/d/1RIQlxcFspH89xvugEeEyvgSG_ABMX3rI/view?usp=drive_link" target="_blank" rel="noreferrer">
+          GET MY CV
+        </a>
+      </button>
+    </div>
+
+    <div className="about-skills">
+      <h2 className="skill-title">Front-End</h2>
+      <div className="skills">
+        <div className="skill">
+          <img
+            src={ReactIcon}
+            alt=""
+          />
+          <p>React</p>
+        </div>
+        <div className="skill">
+          <img
+            src={JavaScript}
+            alt=""
+          />
+          <p>JavaScript</p>
+        </div>
+        
+        <div className="skill">
+          <img
+            src={HTML}
+            alt=""
+          />
+          <p>HTML5</p>
+        </div>
+        <div className="skill">
+          <img
+            src={CSS}
+            alt=""
+          />
+          <p>CSS</p>
+        </div>
+        <div className="skill">
+          <img
+            src={Sass}
+            alt=""
+          />
+          <p>SASS</p>
+        </div>
+        <div className="skill">
+          <img
+            src={BootStrap}
+            alt=""
+          />
+          <p>Bootstrap</p>
+        </div>
+        <div className="skill">
+          <img
+            src={NextJS}
+            alt=""
+          />
+          <p>Next.js</p>
+        </div>
+      </div>
+
+      <h2 className="skill-title">Back-End</h2>
+      <div className="skills">
+        <div className="skill">    
+        </div>
+
+      </div>
+
+      <h2 className="skill-title">Tools</h2>
+      <div className="skills">
+        <div className="skill">
+          <img src={GitLogo} alt="" />
+          <p>Git</p>
+        </div>
+        <div className="skill">
+          <img src={GitHublogo} alt="" />
+          <p>GitHub</p>
+        </div>
+        <div className="skill">
+          <img src={VSCode} alt="" />
+          <p>VS Code</p>
+        </div>
+        <div className="skill">
+          <img src={NPMlogo} alt="" />
+          <p>npm</p>
+        </div>
+      </div>
+     
+      
+      <h2 className="skill-title">Soft Skills</h2>
+      <div className="skills">
+        <p className="soft-skill">Communication</p>
+        <p className="soft-skill">Mentoring</p>
+        <p className="soft-skill">Leadership</p>
+        <p className="soft-skill">Problem Solving</p>
+        <p className="soft-skill">Time Management</p>
+        <p className="soft-skill">Decision Making</p>
+      </div>
+    </div>
    </section>
 );
 
